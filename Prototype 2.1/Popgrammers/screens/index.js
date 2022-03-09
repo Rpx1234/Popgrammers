20d36cdc675d9f9c9f663d7d05da6c39d56e1897
rpx1234 <22777764+Rpx1234@users.noreply.github.com>
2022-03-09 16:36:11 -0500

    updates

--- Prototype 2.1/Popgrammers/screens/index.js
+++
@@ -1,6 +0,0 @@
-import { LoginScreen } from './LoginScreen';
-import { SignupScreen } from './SignupScreen';
-import { HomeScreen } from './HomeScreen';
-import { ForgotPasswordScreen } from './ForgotPasswordScreen';
-
-export { LoginScreen, SignupScreen, HomeScreen, ForgotPasswordScreen };
