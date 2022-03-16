828c56874ad21eb1194fe5c2b96ab65425e4a09d
rpx1234 <22777764+Rpx1234@users.noreply.github.com>
2022-03-16 18:46:48 -0400

    index update

--- Code/AlphaV2/screens/index.js
+++ Code/AlphaV2/screens/index.js
@@ -1,6 +1,5 @@
 import { LoginScreen } from './LoginScreen';
 import { SignupScreen } from './SignupScreen';
 import { HomeScreen } from './HomeScreen';
-import { ForgotPasswordScreen } from './ForgotPasswordScreen';
 
-export { LoginScreen, SignupScreen, HomeScreen, ForgotPasswordScreen };
+export { LoginScreen, SignupScreen, HomeScreen };
