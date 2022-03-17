aec2092adb6e9d730f9c4ce000bc6a3e548a6476
rpx1234 <22777764+Rpx1234@users.noreply.github.com>
2022-03-16 21:56:34 -0400

    Error Code, revised erroring portion
    Forgotpassword, incomplete, temporary fix placed to allow proper loading of app.

--- Code/AlphaV2/screens/index.js
+++ Code/AlphaV2/screens/index.js
@@ -1,5 +1,7 @@
 import { LoginScreen } from './LoginScreen';
 import { SignupScreen } from './SignupScreen';
 import { HomeScreen } from './HomeScreen';
+import { ForgotPasswordScreen } from './ForgotPasswordScreen';
 
-export { LoginScreen, SignupScreen, HomeScreen };
+
+export { LoginScreen, SignupScreen, HomeScreen, ForgotPasswordScreen };
