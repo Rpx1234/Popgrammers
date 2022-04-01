5e849fd1cad5c9bf606f5e3817419718cd43ae9c
rpx1234 <22777764+Rpx1234@users.noreply.github.com>
2022-04-01 19:12:01 -0400

    Inventory Screen updated
    Functions to update and retrieve firebase data added.

--- Code/AlphaV2/screens/InventoryScreen.js
+++ Code/AlphaV2/screens/InventoryScreen.js
@@ -10,14 +10,13 @@ import { useTogglePasswordVisibility } from '../hooks';
 import { loginValidationSchema } from '../utils';
 import { getDatabase, ref, set, update, child, get, onValue } from "firebase/database";
 
-import { collection, getDocs} from "firebase/firestore"; 
+import { collection, getDocs, updateDoc, doc, query, where } from "firebase/firestore"; 
 import Counter from "react-native-counters";
 
 export const InventoryScreen = ({ navigation }) => {
 
 
   const [data, setData] = useState([]);
-  const [count, setCount] = useState(0);
 
   function readInventory(){
     const dbRef = ref(getDatabase());
@@ -38,14 +37,22 @@ export const InventoryScreen = ({ navigation }) => {
 
   }
   
-  function incrementCount(){
-    count = count + 1;
-    setCount(count);
-  }
-  function decrementCount(){
-    count = count - 1;
-    setCount(count);
-  }
+  
+
+  const updateInventory = async (name,newqty) => {
+    var docid;
+    const q = query(collection(db, 'Inventory'), where("name" , "==" , name));
+    const querySnapshot =  await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id);
+      docid = doc.id;
+    });
+    const dbRef = doc(db, 'Inventory', docid);
+    updateDoc(dbRef, {
+      qty: newqty
+    });
+  };
+
 
   
   
@@ -56,7 +63,7 @@ export const InventoryScreen = ({ navigation }) => {
       const inventorySnapshot = await getDocs(collection(db, "Inventory"));
       const inventoryList = inventorySnapshot.docs.map((doc) => doc.data());
       setData(inventoryList);
-      console.log(inventoryList);
+     // console.log(inventoryList);
   };
   getInventory();
 }, [])
@@ -67,28 +74,40 @@ export const InventoryScreen = ({ navigation }) => {
     
    
     <View style={styles.container}>
-      <Text style={styles.screenTitle}>Inventory Screen</Text>
+      <Text style={styles.screenTitle}>Inventory Management</Text>
       {/* Buttons */}
-      <Button style={styles.borderlessButtonContainer} borderless
-        title={'Done with Inventory'}
-      onPress = {() => navigation.navigate('TheaterHomeScreen')} />
-          
       
-        
-      {data.map((data) =>(
+
+     
+      {data.map((data,qty) =>(
         <React.Fragment>
           <View style = {styles.parent}>
-            <Text key = {data} style = {styles.itemText}> {data.name}</Text>            
-            <Counter start = {parseInt(data.qty)} max = {1000}  onChange={(len, type) => {
-              console.log(len, type);
-            }} />
-            <Button style={styles.button} borderless title= {'Submit'}/>
+            <View style = {styles.block}>
+              <Text key = {data} style = {styles.itemText}> {data.name}</Text>            
+
+            </View>
+            <View style = {styles.block} >
+              <Counter  start = {parseInt(data.qty)} max = {1000}  onChange={(len, type) => {
+                console.log(len, type);
+                qty = len;
+              }} />
+            </View>
+            
+            <Button style={styles.button} borderless  title= {'Submit'}   
+              onPress = {() => updateInventory(data.name, qty)}
+            />
+           
 
           </View>
         </React.Fragment>
         
            
       ))}
+     
+      
+      <Button style={styles.borderlessButtonContainer} borderless
+        title={'Return To HomePage'}
+      onPress = {() => navigation.navigate('TheaterHomeScreen')} />
          
 
           
@@ -107,7 +126,7 @@ export const InventoryScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
 	flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: 'black',
     paddingHorizontal: 12,
     alignItems: 'center'
   },
@@ -115,20 +134,11 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   screenTitle: {
-    fontSize: 18,
-    fontWeight: '700',
-    color: Colors.orange,
-    paddingTop: 10
-  },
-  button: {
-    width: '50%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    alignSelf: 'center',
-    marginTop: 8,
-    backgroundColor: Colors.orange,
-    padding: 10,
-    borderRadius: 8,
+    fontSize: 25,
+    fontWeight: '900',
+    color: '#fe4f02',
+    paddingTop: 10,
+    paddingBottom: 20
   },
   buttonText: {
     fontSize: 20,
@@ -141,27 +151,32 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   itemText: {
-    fontSize: 18,
-    fontWeight: '700',
-    color: Colors.red,
-    paddingTop: 10,
-    marginRight: 20
+    fontSize: 20,
+    fontWeight: '500',
+    color: '#ff9361',
+    
+    
   },
   parent: {
     
     flexDirection: "row",
     
   },
+  block: {
+    flex: 3,
+    margin: 6,
+  },
+  
   button: {
-
+    
     width: '20%',
-    justifyContent: 'flex-end',
     alignItems: 'center',
-    alignSelf: 'flex-end',
     marginLeft: 10,
-    backgroundColor: Colors.orange,
+    backgroundColor: '#fe4f02',
     padding: 4,
     borderRadius: 8,
+    height: 30,
+    color: '#f6f6f6'
   }
   
   });
