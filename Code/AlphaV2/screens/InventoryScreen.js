31d5148cd43f59ad1dd128df62dd69dfec7870bc
macncheez67 <99365867+macncheez67@users.noreply.github.com>
2022-04-06 16:17:06 -0400

    this

--- Code/AlphaV2/screens/InventoryScreen.js
+++ Code/AlphaV2/screens/InventoryScreen.js
@@ -66,7 +66,7 @@ export const InventoryScreen = ({ navigation }) => {
      // console.log(inventoryList);
   };
   getInventory();
-}, [])
+}, [])                                                                                        
 
 
   
--- Code/AlphaV2/screens/ProgressRecieptScreen.js
+++ Code/AlphaV2/screens/ProgressRecieptScreen.js
@@ -1,33 +1,116 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { Text, StyleSheet } from 'react-native';
 import { Formik } from 'formik';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 import { View, TextInput, Logo, Button, FormErrorMessage } from '../components';
-import { Images, Colors, auth } from '../config';
+import { Images, Colors, auth, db } from '../config';
 import { useTogglePasswordVisibility } from '../hooks';
 import { loginValidationSchema } from '../utils';
+import { getDatabase, ref, set, update, child, get, onValue } from "firebase/database";
 
-
+import { collection, getDocs, updateDoc, doc, query, where } from "firebase/firestore"; 
+import Counter from "react-native-counters";
 
 export const ProgressRecieptScreen = ({ navigation }) => {
   
-  return (
+  const [data, setData] = useState([]);
 
-    <View style={styles.container}>
-      <Text style={styles.screenTitle}>In Progress</Text>
-      {/* Buttons */}
-          <Button style={styles.borderlessButtonContainer} borderless
-          title={'Done'}
-          onPress = {() => navigation.navigate('RecieptScreen')} />
-    </View>
-  );
+  function readOrders(){
+    const dbRef = ref(getDatabase());
+    get(child(dbRef, `orders/`)).then((snapshot) => {
+      if (snapshot.exists()) {
+        //inventory = (snapshot.val());
+        console.log(snapshot.val());
+        console.log("test");
+
+      } else {
+        console.log("No data available");
+     }
+    }).catch((error) => {                            
+      console.error(error);
+    });
+
+
+  }
+  
+  
+
+  const updateOrders = async (Card_Number) => {
+    var docid;
+    const q = query(collection(db, 'Orders'), where("Card_Number" , "==" , Card_Number));
+    const querySnapshot =  await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id);
+      docid = doc.id;
+    });
+    const dbRef = doc(db, 'Orders', docid);
+    updateDoc(dbRef, {
+      Card_Number: Card_Number
+    });
+  };
+
+
+  
+  
+
+
+  useEffect(() => {
+    const getOrders = async () => {
+      const ordersSnapshot = await getDocs(collection(db, "Orders"));
+      const ordersList = ordersSnapshot.docs.map((doc) => doc.data());
+      setData(ordersList);
+      //console.log(ordersList);
+  };
+  getOrders();
+}, [])
+  
+return (
+    
+   
+  <View style={styles.container}>
+    <Text style={styles.screenTitle}>In Progress</Text>
+    {/* Buttons */}
+    
+    <View style = {styles.parent}>
+            <Text key = {data} style = {styles.itemText}> {'Order #'}</Text>            
+            <Text key = {data} style = {styles.itemText}> {'Date'}</Text>    
+            <Text key = {data} style = {styles.itemText}> {'box'}</Text>    
+        </View>
+   
+    {data.map((data,Card_Number) =>(
+      <React.Fragment>
+  
+        <View style = {styles.parent}>
+            <Text key = {data} style = {styles.itemText}> {data.OrderNum}</Text>            
+            <Text key = {data} style = {styles.itemText}> {data.RecievedDate}</Text>    
+            <Text key = {data} style = {styles.itemText}> {'box'}</Text>    
+        </View>
+      </React.Fragment>
+      
+         
+    ))}
+  
+    
+    <Button style={styles.borderlessButtonContainer} borderless
+      title={'Return To Receipts'}
+    onPress = {() => navigation.navigate('RecieptScreen')} />
+       
+
+        
+          
+        
+
+
+
+  </View>
+  
+);
 };
 
 
-          
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({ 
   container: {
 	flex: 1,
     backgroundColor: 'black',
@@ -38,20 +121,11 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   screenTitle: {
-    fontSize: 18,
-    fontWeight: '700',
-    color: Colors.orange,
-    paddingTop: 10
-  },
-  button: {
-    width: '50%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    alignSelf: 'center',
-    marginTop: 8,
-    backgroundColor: Colors.orange,
-    padding: 10,
-    borderRadius: 8,
+    fontSize: 25,
+    fontWeight: '900',
+    color: '#fe4f02',
+    paddingTop: 10,
+    paddingBottom: 20
   },
   buttonText: {
     fontSize: 20,
@@ -62,5 +136,32 @@ const styles = StyleSheet.create({
   borderlessButtonContainer: {
     marginTop: 16, 
     alignItems: 'center',
+  },
+  itemText: {
+    fontSize: 20,
+    fontWeight: '500',
+    color: '#ff9361',
+    
+  },
+  parent: {
+    
+    flexDirection: "row",
+    
+  },
+  block: {
+    flex: 3,
+    margin: 6,
+  },
+  
+  button: {
+    
+    width: '20%',
+    alignItems: 'center',
+    marginLeft: 10,
+    backgroundColor: '#fe4f02',
+    padding: 4,
+    borderRadius: 8,
+    height: 30,
+    color: '#f6f6f6'
   }
   });
